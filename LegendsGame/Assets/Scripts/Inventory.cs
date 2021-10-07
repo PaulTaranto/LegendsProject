5075cb47e8ed714c0c6dae198b63782384ca1b60
klin0038 <klin0038@flinders.edu.au>
2021-10-08 10:21:30 +1030

    Added pickup script for wands
    Wands can now be picked up and added to inventory.
    Currently only have 1 inventory slot.
    Add pickup script to item on ground to allow player to pick it up.
    Only works when player is enabled at start, not with cloned player

---
+++ LegendsGame/Assets/Scripts/Inventory.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    //private Item item;
+    //private List<Item> itemList;
+    public bool[] isFull;
+    public GameObject[] slots;
+    
+
+    /*public Inventory() {
+        itemList = new List<Item>();
+
+        AddItem(new Item { itemType = Item.ItemType.Apple});
+        Debug.Log(itemList.Count);
+        Debug.Log("Inventory");
+    }*/
+
+    /*//Adds item to list when picked up
+    public void AddItem(Item item) {
+        itemList.Add(item);
+    }*/
+}
---
+++ LegendsGame/Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    
+    public enum ItemType {
+        Apple,
+        GoblinToe,
+        SlimeBladder,
+        Crystal,
+        Egg,
+        Potion,
+    }
+
+    public ItemType itemType;
+
+}
