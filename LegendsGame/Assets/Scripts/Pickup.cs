58a9ae30c04e1d15fd864c1b5d6ddf67f903f71f
klin0038 <klin0038@flinders.edu.au>
2021-10-09 15:50:59 +1030

    Adjusted script to pickup wand on button press
    Wands are not picked up on button press (Curerntly Z), instead of on collision.

--- LegendsGame/Assets/Scripts/Pickup.cs
+++ LegendsGame/Assets/Scripts/Pickup.cs
@@ -7,6 +7,7 @@ public class Pickup : MonoBehaviour
 
     private Inventory inventory;
     public GameObject itemB;
+    private bool canPickupWand;
 
     // Start is called before the first frame update
     /*private void Start()
@@ -21,7 +22,14 @@ public class Pickup : MonoBehaviour
         {
             inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
         }
+
+        //if button pressed pickup wand (currently set to Z)
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            PickupWand();
+        }
     }
+    
+    /*
     //Runs through the inventory and checks if there is any free slots if collision with item occurs
     //Item is added to Wand slot if free
     void OnTriggerEnter2D(Collider2D other) {
@@ -48,6 +56,50 @@ public class Pickup : MonoBehaviour
                 }
             }
         }
+    }*/
+
+    //Allow pickup when on object
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("Collison Active");
+            canPickupWand = true;
+        }
+    }
+
+    //Turn off pickup when not on object
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("Collision Not Active");
+            canPickupWand = false;
+        }
+    }
+    
+    //Runs through the inventory and checks if there is any free slots if collision with item occurs
+    //Item is added to Wand slot if free
+    void PickupWand() {
+        if (canPickupWand) {
+            for (int i = 0; i < inventory.slots.Length; i++) {
+                if (inventory.isFull[i] == false) {
+                    inventory.isFull[i] = true;
+                    //Check if item is going into second slot.
+                    //Changes size of item in slot to match the box.
+                    if (i == 1) {
+                        itemB = (GameObject)Instantiate(itemB, inventory.slots[i].transform, false);
+                        itemB.transform.localScale = new Vector2(0.6f, 0.6f);
+                        Destroy(gameObject);
+                        break;
+                        
+                    }
+                    else {
+                        itemB = (GameObject)Instantiate(itemB, inventory.slots[i].transform, false);
+                        Destroy(gameObject);
+                        break;
+                    }
+                    
+                }
+            }
+        }
+        
     }
 
 }
