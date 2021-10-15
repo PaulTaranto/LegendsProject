aa658cfb6f8b95e84df7842b53155d6cfedb8747
Monkey4ce <88819370+Monkey4ce@users.noreply.github.com>
2021-10-15 20:54:17 +1030

    Add files via upload

---
+++ LegendsGame/Assets/Scripts/MoveFoward.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFoward : MonoBehaviour
+{
+    public float speed = 40;
+    private Rigidbody playerRb;
+    private Collider m_Collider;
+    public float DestroyedTimer = 3;
+    void Start()
+    {
+        GameObject Rotator = GameObject.Find("Weapons & Rotator");
+        playerRb = GetComponent<Rigidbody>();
+        m_Collider = GetComponent<Collider>();
+        StartCoroutine(Limit());
+    }
+    void Update()
+    {
+        //just to walk forward
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+    private IEnumerator Limit()
+    {
+        yield return new WaitForSeconds(DestroyedTimer);
+        Destroy(gameObject);
+    }
+}
