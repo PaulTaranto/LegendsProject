7df9add6e9ffaa7c2881f5f1046abf58d12b2480
Monkey4ce <88819370+Monkey4ce@users.noreply.github.com>
2021-10-19 00:32:02 +1030

    Add files via upload

---
+++ LegendsGame/Assets/Scripts/SwapWeapons.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapWeapons : MonoBehaviour
+{
+    public GameObject[] Equiped;
+    public int Selected;
+
+    public GameObject[] Weapons;
+    public int WeaponNumber;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Selected % 2 == 0)
+        {
+            Equiped[0].SetActive(true);
+            Equiped[1].SetActive(false);
+        }
+        if (Selected % 2 == 1)
+        {
+            Equiped[0].SetActive(false);
+            Equiped[1].SetActive(true);
+        }
+
+        //When Tab it put down
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selected += 1;
+        }
+    }
+    
+    public void Change()
+    {
+        Equiped[Selected % 2] = Weapons[WeaponNumber];
+
+        if (Equiped[0] != Weapons[WeaponNumber] || Equiped[Equiped.Length] != Weapons[WeaponNumber])
+        {
+            Equiped[Selected % 2] = Instantiate(Weapons[WeaponNumber], transform.position, transform.rotation);
+        }
+        
+    }
+}
